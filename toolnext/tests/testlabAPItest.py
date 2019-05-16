e4f7c74efa599820d4cf6e5db7b53243ebc57b1a
Jan Hutar <jhutar@redhat.com>
2019-05-16 22:51:52 +0200

    Improved Testlab import test

--- toolnext/tests/testlabAPItest.py
+++ toolnext/tests/testlabAPItest.py
@@ -20,6 +20,11 @@ logging.basicConfig(
 
 class testlabAPItest(unittest.TestCase):
 
+    PROJECT_KEY = 'TLABTMPL'
+    PROJECT_NAME = 'ToolNext'
+    USER = 'smanager1'
+    API_KEY = '550e8400-e29b-41d4-a716-44665544'
+
     def setUp(self):
         """
         Set variables, load the browser
@@ -35,108 +40,80 @@ class testlabAPItest(unittest.TestCase):
 
     def test_login(self):
         # This works
-        company = 'ToolNext'
-        apiKey = '550e8400-e29b-41d4-a716-44665544'
-        res = requests.get('https://rhtoolnext.melioratestlab.com/api/defect/ToolNext',
-                           auth=('user', apiKey))
+        res = requests.get('https://rhtoolnext.melioratestlab.com/api/defect/%s' % self.PROJECT_NAME,
+                           auth=('user', self.API_KEY))
         logging.debug("%s - %s" % (res.status_code, res.text))
         #assert res.ok   # this fails with 404
 
         # Works to get all test cases in project=TLABTMPL
-        res = requests.get('https://rhtoolnext.melioratestlab.com/api/testcase/TLABTMPL',
-                           auth=('user', apiKey))
+        res = requests.get('https://rhtoolnext.melioratestlab.com/api/testcase/%s' % self.PROJECT_KEY,
+                           auth=('user', self.API_KEY))
         logging.debug("%s - %s" % (res.status_code, res.text))
         assert res.ok
 
         # Works to get the single test case data
-        res = requests.get('https://rhtoolnext.melioratestlab.com/api/testcase/TLABTMPL/227091',
-                           auth=('user', apiKey))
+        res = requests.get('https://rhtoolnext.melioratestlab.com/api/testcase/%s/227091' % self.PROJECT_KEY,
+                           auth=('user', self.API_KEY))
         logging.debug("%s - %s" % (res.status_code, res.text))
         assert res.ok
 
-    def test_aaa(self):
-        apiKey = '550e8400-e29b-41d4-a716-44665544'
+    def test_upload_results_data(self):
+        count_of_results = 3500
+
+        def _generate_results(count):
+            results = []
+            for i in range(count):
+                test_name = "myAutomatedTest%s" % i
+                result = i % 3
+                comment = "Test %s with result %s" % (test_name, result)
+                results.append({
+                    "mappingId": test_name,
+                    "result": result,
+                    "started": 1487250019387 + i,
+                    "run": 1487250019987 + i + 1,
+                    "comment": comment,
+                })
+            return results
+
         data = {
-          "projectKey": "TLABTMPL",
-          "testRunTitle": "Automated run 3",
+          "projectKey": self.PROJECT_KEY,
+          "testRunTitle": "Automated run %s" % time.time(),
           "comment": "Some comment text bound to the test run",
           "status": 3,
-          "user": "smanager1",
+          "user": self.USER,
+
           "milestoneIdentifier": "A2",
           "milestoneTitle": "myAutomatedMilestoneTitle 2",
           "testTargetTitle": "myAutomatedTestTargetTitle",
           "testEnvironmentTitle": "myAutomatedTest",
           "tags": "myAutomatedTag1 myAutomatedTag2",
-          "testCaseMappingField": "Automated",
+
           "addIssues": False,
           "mergeAsSingleIssue": False,
           "reopenExistingIssues": False,
-          "assignIssuesToUser": "stester1",
+          "assignIssuesToUser": self.USER,
+
+          "testCaseMappingField": "Automated",
           "importTestCases": True,
           "importTestCasesRootCategory": "import",
+
           #"robotCatenateParentKeywords": true
           #"xml": "<jUnit/xUnit XML content or Robot Framework output XML>",
           #"xmlFormat": "junit | robot",
-          "results": [
-            {
-              "mappingId": "myAutomatedTest1",
-              "result": 1,
-              "started": 1487250019387,
-              "run": 1487250019987,
-              "runBy": "selenium-agent",
-              "comment": "Timeout occurred",
-              "steps": [
-                {
-                  "result": 1,
-                  "description": "Navigate to the site",
-                  "expected": "Front page of the site opens up",
-                  "comment": "the loading time of the page was nicely snappy",
-                  "custom1": "A value for first custom field",
-                },
-                {
-                  "result": 2,
-                  "description": "...",
-                  "expected": "...",
-                  "comment": "...",
-                  "custom1": "...",
-                }
-              ]
-            },
-            {
-              "mappingId": "myAutomatedTest2",
-              "result": 3,
-              "started": 1558005552000,
-              "run": 1558005752000,
-              "runBy": "selenium-agent",
-              "comment": "...",
-              "steps": [
-                {
-                  "result": 4,
-                  "description": "...",
-                  "expected": "...",
-                  "comment": "...",
-                  "custom1": "...",
-                },
-                {
-                  "result": 5,
-                  "description": "...",
-                  "expected": "...",
-                  "comment": "...",
-                  "custom1": "...",
-                }
-              ]
-            }
-          ],
+          "results": _generate_results(count_of_results),
         }
         headers = {
             'Content-Type': 'application/json;charset=UTF-8',
             'Accept': 'application/json;charset=UTF-8',
         }
+        logging.debug("Sending data: %s" % data)
+        before = time.time()
         res = requests.put('https://rhtoolnext.melioratestlab.com/api/testresult',
-                           auth=('user', apiKey), json=data, headers=headers)
-        print(res.ok)
-        print(res.status_code)
-        print(res.text)
+                           auth=('user', self.API_KEY), json=data, headers=headers)
+        after = time.time()
+        logging.info("Upload of %s results via data took %.2f secods succesfull? %s" \
+            % (count_of_results, after-before, res.ok))
+        assert res.ok, "Response text: %s" % res.text
 
 
 if __name__ == "__main__":   # allows unittest to start by running this class file
