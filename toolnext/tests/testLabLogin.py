8cc48c2b76455d9778b661a93d9a7aecc451d327
Jan Hutar <jhutar@redhat.com>
2019-05-19 21:58:43 +0200

    Bit of cleanup; Get rid of CLI opts as these are not used anyways

--- toolnext/tests/testLabLogin.py
+++ toolnext/tests/testLabLogin.py
@@ -31,8 +31,8 @@ class testLabLogin(unittest.TestCase):
     def setUp(self):
         # set variables, load the browser
         logging.info("Function Setup()")
-        self.url = "https://rhtoolnext.melioratestlab.com/testlab/"
         # the defaults
+        self.url = "https://rhtoolnext.melioratestlab.com/testlab/"
         self.user = 'stester1'
         self.password = "rhtoolnext"
         self.browser = "ff"
@@ -55,15 +55,15 @@ class testLabLogin(unittest.TestCase):
                 self.password = self.testdatadict['password']
             if ('url' in self.testdatadict):
                 self.url = self.testdatadict['url']
+            if ('browser' in self.testdatadict):
+                self.browser = self.testdatadict['browser']
 
         logging.info("Launching browser -> %s" % self.browser)
-        logging.info("loading url %s" % self.url)
-        logging.info("looping %s times using id=%s and password=%s" % (str(self.myLoops), self.user, self.password))
+        logging.info("Loading url %s" % self.url)
+        logging.info("Looping %s times using id=%s and password=%s" % (str(self.myLoops), self.user, self.password))
 
     def tearDown(self):
-        # close the browser
         logging.debug("Function tearDown()")
-        # self.driver.close()
 
     def test_login(self):
         for z in range(1, self.myLoops+1):
@@ -76,13 +76,12 @@ class testLabLogin(unittest.TestCase):
             self.loader = Page(self.driver)
             self.loginPage = LoginPage1(self.driver)
             self.logoutPage = LogoutPage(self.driver)
-            logging.info("Test logging into Xray")
-            logging.info("loading url %s" % self.url)
-            self.loader.open(self.url)   # or this
+            logging.info("Test logging into Testlab")
+            logging.info("Loading url %s" % self.url)
+            self.loader.open(self.url)
             self.loginPage.login_with_valid_userTL(self.user, self.password)
             time.sleep(1)
             logging.info("Closing browser")
-            # self.driver.quit()
             self.driver.close()
             # for some reason TL thinks something has changed we get
             # a warning dialog
@@ -93,17 +92,5 @@ class testLabLogin(unittest.TestCase):
 
 
 if __name__ == "__main__":   # allows unittest to start
-    argv = sys.argv[1:]
-
-    # command line parser
-    parser = OptionParser(usage='%prog [options] ', version='0.1',)
-    parser.add_option("-n", "--loops", dest="myLoops", default=10,
-                      help='Defines how many cycles')
-
-    # read the command line parameters now
-    (options, args) = parser.parse_args(argv)
-    myLoops = options.loops
     logging.info("Command Line: %s" % sys.argv)
-    logging.info(options.loops)
-
     unittest.main()
