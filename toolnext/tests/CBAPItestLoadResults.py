bca5eb4931512990f6a693c01026fdd97645a790
brian12559 <bmurray@redhat.com>
2019-08-27 08:20:06 -0400

    put load in a loop

--- toolnext/tests/CBAPItestLoadResults.py
+++ toolnext/tests/CBAPItestLoadResults.py
@@ -25,73 +25,74 @@ class codeBeamerAPItest(unittest.TestCase):
 
     def test_login(self):
 
-        #1st create the test run based on the test set
-        res = requests.post(
-            "https://codebeamer-devel.hosts.prod.upshift.rdu2.redhat.com:8443/cb/rest/testRun?testSetId=3140",
-            auth=('stester1', 'rhtoolnext1'), verify=False)
+        for q in range(1,10):
+            #1st create the test run based on the test set
+            res = requests.post(
+                "https://codebeamer-devel.hosts.prod.upshift.rdu2.redhat.com:8443/cb/rest/testRun?testSetId=3140",
+                auth=('stester1', 'rhtoolnext1'), verify=False)
 
-        print res.status_code
-        marker = res.text.find("item") + 5
-        testRun = res.text[marker:marker + 5]
-        print testRun
-       # print res.text
+            print res.status_code
+            marker = res.text.find("item") + 5
+            testRun = res.text[marker:marker + 5]
+            print testRun
+           # print res.text
 
-        #for generating random pass fails
-        myRan = round(random.random() * 10) + 1
-        logging.info(myRan)
+            #for generating random pass fails
+            myRan = round(random.random() * 10) + 1
+            logging.info(myRan)
 
-        # this gets test case with id = 1001 information, can see this info on screen when you open test case its 3141
-        # small one is 5149
-        #res = requests.get("https://codebeamer-devel.hosts.prod.upshift.rdu2.redhat.com:8443/cb/api/v2/item/49022",
-                           #auth=('bmurray', 'bandg916'), verify=False)
-        res = requests.get("https://codebeamer-devel.hosts.prod.upshift.rdu2.redhat.com:8443/cb/api/v2/item/%s" % testRun,
-                           auth=('bmurray', 'bandg916'), verify=False)
+            # this gets test case with id = 1001 information, can see this info on screen when you open test case its 3141
+            # small one is 5149
+            #res = requests.get("https://codebeamer-devel.hosts.prod.upshift.rdu2.redhat.com:8443/cb/api/v2/item/49022",
+                               #auth=('bmurray', 'bandg916'), verify=False)
+            res = requests.get("https://codebeamer-devel.hosts.prod.upshift.rdu2.redhat.com:8443/cb/api/v2/item/%s" % testRun,
+                               auth=('bmurray', 'bandg916'), verify=False)
 
-        print res.status_code
-        print res.text
-        tests = res.json()
+            print res.status_code
+            print res.text
+            tests = res.json()
 
-        my_dict = json.loads(res.text)
+            my_dict = json.loads(res.text)
 
-        # gotta be a better way
-        myIDs = []
+            # gotta be a better way
+            myIDs = []
 
-        for x in my_dict.get('customFields'):
-            for key1, value1 in x.iteritems():
-                if key1 == 'values':
-                    for z in x.get('values'):
-                        for q in z:
-                            for x_id in q.get('values'):
-                                # and finally
-                                #print x_id.get("id")
-                                myIDs.append(x_id.get("id"))
+            for x in my_dict.get('customFields'):
+                for key1, value1 in x.iteritems():
+                    if key1 == 'values':
+                        for z in x.get('values'):
+                            for q in z:
+                                for x_id in q.get('values'):
+                                    # and finally
+                                    #print x_id.get("id")
+                                    myIDs.append(x_id.get("id"))
 
-        # this gets test case with id = 1001 information, can see this info on screen when you open test case
-        testdata = {}
-        #mylist = []
-        xNum = 0
-        for myTest in myIDs:
-            if xNum == 0:
-                testdata = {"%s" % str(myTest): {"success": "true"}}
-            xNum = xNum + 1
-            y = xNum/myRan
-            q=int(xNum/myRan)
-            if (y-q==0) and xNum > 1:
-                testdata.update({"%s" % str(myTest) : {"success": "false"}})
-                #mylist.append({"%s" % str(myTest) : {"success": "false"}})
-            else:
-                testdata.update({"%s" % str(myTest): {"success": "true"}})
-                #mylist.append({"%s" % str(myTest) : {"success": "true"}})
-        #testdata.update({"" : mylist})
-        time.sleep(2)
+            # this gets test case with id = 1001 information, can see this info on screen when you open test case
+            testdata = {}
+            #mylist = []
+            xNum = 0
+            for myTest in myIDs:
+                if xNum == 0:
+                    testdata = {"%s" % str(myTest): {"success": "true"}}
+                xNum = xNum + 1
+                y = xNum/myRan
+                q=int(xNum/myRan)
+                if (y-q==0) and xNum > 1:
+                    testdata.update({"%s" % str(myTest) : {"success": "false"}})
+                    #mylist.append({"%s" % str(myTest) : {"success": "false"}})
+                else:
+                    testdata.update({"%s" % str(myTest): {"success": "true"}})
+                    #mylist.append({"%s" % str(myTest) : {"success": "true"}})
+            #testdata.update({"" : mylist})
+            time.sleep(2)
 
-        res = requests.post("https://codebeamer-devel.hosts.prod.upshift.rdu2.redhat.com:8443/cb/rest/testRun/%s/result" % testRun,
-                        auth=('bmurray', 'bandg916'), verify=False,
-                             headers={'content-type': 'application/json'},
-                             json=testdata)
-        #
-        print res.status_code
-        print res.text
+            res = requests.post("https://codebeamer-devel.hosts.prod.upshift.rdu2.redhat.com:8443/cb/rest/testRun/%s/result" % testRun,
+                            auth=('bmurray', 'bandg916'), verify=False,
+                                 headers={'content-type': 'application/json'},
+                                 json=testdata)
+            #
+            print res.status_code
+            print res.text
 
     def atest_createTR(self):
 
